6b41f424077a2b648021b64bf0d7a4d7052204fd
Gary Hou <ghou@gofundme.com>
2016-07-21 23:30:55 -0700

    working v1

--- fb_garybot/urls.py
+++ fb_garybot/urls.py
@@ -1,2 +1,6 @@
 from django.conf.urls import include, url
-urlpatterns = []
+from .views import GaryBotView
+
+urlpatterns = [
+    url(r'^b345e21cca34732679d2804b16ac4d376c412817887a689305/?$', GaryBotView.as_view())
+]
--- fb_garybot/views.py
+++ fb_garybot/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+class GaryBotView(generic.View):
+    def get(self, request, *args, **kwargs):
+        if self.request.GET['hub.verify_token'] == 'thisisgarysecretbot':
+            return HttpResponse(self.request.GET['hub.challenge'])
+        return HttpResponse("100460735")
