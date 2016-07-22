8c4015ada3003094f6ac3f46ac0cb4325ab0233b
Gary Hou <ghou@gofundme.com>
2016-07-22 05:20:32 -0700

    added persiste menu

--- fb_garybot/views.py
+++ fb_garybot/views.py
@@ -7,6 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views import generic
 from django.http.response import HttpResponse
 
+ACCESS_TOKEN = 'EAAWXRgus2csBAFpHBukNcnxIVND4f2wxIHBIfAmz1eoPn7t3Ic2UeN2FJppmbtJeYdDnkcpADMl4DKC85yE37IDJWugsKDTZAXZBDVMq5fYwDiJVW2y4WHSpqQ9ppZAxlrLcDykWAWWrNoC4MCsdSv8GxwACLgXS2ERQ40ceAZDZD'
 
 facts = {
     'gary facts' : [
@@ -24,12 +25,22 @@ facts = {
     ],
 }
 def post_facebook_message(fbid, recevied_message):
+    # user data
     user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
     user_details_params = {
         'fields':'first_name,last_name,profile_pic',
-        'access_token':'EAAWXRgus2csBAFpHBukNcnxIVND4f2wxIHBIfAmz1eoPn7t3Ic2UeN2FJppmbtJeYdDnkcpADMl4DKC85yE37IDJWugsKDTZAXZBDVMq5fYwDiJVW2y4WHSpqQ9ppZAxlrLcDykWAWWrNoC4MCsdSv8GxwACLgXS2ERQ40ceAZDZD'
+        'access_token': ACCESS_TOKEN
     }
     user_details = requests.get(user_details_url, user_details_params).json()
+
+    # persist menu
+    persist_menu_url = "https://graph.facebook.com/v2.6/me/thread_settings?access_token=%s" % ACCESS_TOKEN
+    pprint(requests.post(
+        persist_menu_url,
+        headers={"Content-Type": "application/json"},
+        data=persist_menu_data).json())
+
+    # user text input
     response_medium = 'text'
     # Remove all punctuations, lower case the text and split it based on space
     tokens = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message).lower().split()
@@ -40,21 +51,28 @@ def post_facebook_message(fbid, recevied_message):
             break
     if recevied_message in facts:
         response_text = random.choice(facts[recevied_message.lower()])
-    elif recevied_message.lower() == 'hello':
+    elif 'hello' in tokens:
         response_text = 'Hello ' + user_details['first_name'] + '! How are you feeling today?'
     elif 'pokemon' in tokens:
         response_text = 'There are 250 pokemons in total.'
     elif 'nearby' in tokens:
-        response_text = 'there are 12 campaigns near your location.'
+        response_medium = 'template'
+    # TODO: COMPLETE TRENDING DATA
+    elif 'trending' in tokens:
         response_medium = 'template'
     elif not response_text:
         response_text = "Sorry " + user_details['first_name'] + ", I don't understand that. Can you rephrase please?"
     
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAAWXRgus2csBALfz5S21sFpZAchEEuYHiCo8OwzViHzFIrXs06zdFzuN1HskDAVoe1OFxG7JTZBRQ0imQjZApZA7IjjQS4UZAKgbBWSiiXaZBfokVNwMlHyMqfSXpxm3WuBT7kiZBL0hdh8WDz09JloZCbnEbBZAKAZAZC9RuqnWJEZBlAZDZD'
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % ACCESS_TOKEN
     if response_medium == 'text':
-        response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":response_text}})
-        # status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
-        # pprint(status.json())
+        response_msg = json.dumps({
+            "recipient" : {
+                "id":fbid
+            },
+            "message":{
+                "text":response_text
+            }
+        })
     elif response_medium == 'template':
         response_msg = json.dumps({
             "recipient" : {
@@ -96,6 +114,17 @@ class GaryBotView(generic.View):
                         post_facebook_message(message['sender']['id'], message['message']['text'])
         return HttpResponse()
 
+persist_menu_data = {
+    "setting_type" : "call_to_actions",
+    "thread_state" : "existing_thread",
+    "call_to_actions" : [
+        {
+            "type" : "web_url",
+            'title' : "Go to gofundme.com",
+            "url" : "https://gofundme.com"
+        }
+    ]
+}
 
 nearby_data = {
                 "attachment" : {
